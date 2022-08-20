2c024a1bfbe46ca2de0353120622466ac918b565
maximandreev <maxands@yandex.ru>
2022-08-20 19:57:12 +0300

    Task 2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿void fillArray(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+
+void printArray(int[] array)
+{
+    foreach (int element in array)
+    {
+        Console.Write(element+" ");
+    }
+}
+
+
+int length = new Random().Next(5, 11);
+int[] array = new int[length];
+fillArray(array);
+printArray(array);
+int sum = 0;
+for (int i=1; i<array.Length; i+=2)
+{
+    sum += array[i];
+}
+Console.WriteLine("\n"+sum);
