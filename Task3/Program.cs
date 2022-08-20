cc3fa2860d85529441095195d62e97715b6eef68
maximandreev <maxands@yandex.ru>
2022-08-20 20:01:39 +0300

    Task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿void fillArray(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+
+void printArray(int[] array)
+{
+    foreach (int element in array)
+    {
+        Console.Write(element+" ");
+    }
+}
+
+
+int length = new Random().Next(5, 11);
+int[] array = new int[length];
+fillArray(array);
+printArray(array);
+int max = array[0];
+int min = array[1];
+foreach (int element in array)
+{
+    if (element > max)
+    {
+        max = element;
+    }
+    else if (element < min)
+    {
+        min = element;
+    }
+}
+Console.WriteLine("\n"+(max-min));
