00e03e1d807d9b7d3f7456df3aac43d52976f4eb
maximandreev <maxands@yandex.ru>
2022-08-20 15:26:02 +0300

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿void fillArray(int[] array)
+{
+    int x = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        x = new Random().Next(100, 1000);
+        array[i] = x;
+    }
+}
+
+
+void printArray(int[] array)
+{
+    foreach (int element in array)
+    {
+        Console.Write(element+" ");
+    }
+}
+
+
+int length =  new Random().Next(5,11);
+int[] array = new int[length];
+fillArray(array);
+printArray(array);
+int count = 0;
+foreach (int element in array)
+{
+    if (element % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("\n"+count);
